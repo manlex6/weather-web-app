f34d85ac5f145e89eb91e202adcaf31fdc012ce8
manlex6 <manlex6@gmail.com>
2021-12-14 17:05:51 -0500

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,26 @@
-console.log('Client side rendering');
+const toJson = (data) => data.json();
 
-const toJson = data => data.json();
+const $weatherForm = document.querySelector('form');
+const $search = document.querySelector('input');
+const $message1 = document.querySelector('.message-1');
+const $message2 = document.querySelector('.message-2');
 
+$weatherForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  $message1.textContent = '';
+  $message2.textContent = '';
 
-
- const $weatherForm = document.querySelector('form');
- const $search = document.querySelector('input');
- const $message1 = document.querySelector('.message-1')
- const $message2 = document.querySelector('.message-2')
-
- $weatherForm.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    $message1.textContent = ''
-    $message2.textContent = ''
-
-    fetch(`http://localhost:3000/weather?address=${$search.value}`)
+  fetch(`http://localhost:3000/weather?address=${$search.value}`)
     .then(toJson)
-    .then(data => {
-        if (data.error){
-            console.log(data.error)
-            $message1.textContent = data.error;         
-        }
-        else {
-            console.log(data.location)
-            $message1.textContent = data.location;
-            $message2.textContent = data.weather.weather_descriptions[0]         
-            console.log(data.weather)
-        }
-    })    
- })
+    .then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        $message1.textContent = data.error;
+      } else {
+        console.log(data.location);
+        $message1.textContent = data.location;
+        $message2.textContent = data.weather.weather_descriptions[0];
+        console.log(data.weather);
+      }
+    });
+});
